385f1bc044cdadd4aab4540b95f6245212b07a6a
Stuart Croy <stuartcroy@mac.com>
2023-10-11 20:10:11 +0100

    parser: clean-up

--- src/parser.rs
+++ src/parser.rs
@@ -6,131 +6,111 @@ use crate::ty::Type;
 
 use Token::*;
 
-// [Token] needs copy
-// update Token to not use String
-// but will introduce lifecycle, so may need to implement Parser traits
-// anyway
-//
-
 peg::parser! {
     pub grammar parser<'a>() for [Token<'a>] {
 
-        pub rule simple_exp( st: () ) -> Box<Syntax> = precedence!{
-            [LParen] [RParen]           { Box::new( Syntax::Unit ) }
-            [Bool(x)]                   { Box::new( Syntax::Bool(x) ) }
-            [Int(x)]                    { Box::new( Syntax::Int(x) ) }
-            [Float(x)]                  { Box::new( Syntax::Float(x) ) }
+        pub rule simple_exp(st:()) -> Box<Syntax> = precedence!{
+            [LParen] [RParen]           { Box::new(Syntax::Unit) }
+            [Bool(x)]                   { Box::new(Syntax::Bool(x)) }
+            [Int(x)]                    { Box::new(Syntax::Int(x)) }
+            [Float(x)]                  { Box::new(Syntax::Float(x)) }
             // FIXME: create id
             [Ident(n)]
-                { Box::new( Syntax::Var( String::from( n ) ) ) }
-            [LParen] e:exp( st ) [RParen]   { e }
-            e0:(@) [Dot] [LParen] e1:exp( st ) [RParen]
-                { Box::new( Syntax::Get( e0, e1 ) ) }
+                { Box::new(Syntax::Var(String::from(n))) }
+            [LParen] e:exp(st) [RParen]   { e }
+            e0:(@) [Dot] [LParen] e1:exp(st) [RParen]
+                { Box::new(Syntax::Get(e0, e1)) }
         }
 
-        pub rule exp( st : () ) -> Box<Syntax> = precedence!{
-            [If] x:(exp( st )) [Then] y:(exp( st )) [Else] z:(exp( st ))
-                { Box::new( Syntax::If( x, y, z ) ) }
-            [Let] [Rec] [Ident(n)] formal_args:[Ident(_)]+ [Equal] e0:exp( st )
-                [In] e1:exp( st )
+        pub rule exp(st : ()) -> Box<Syntax> = precedence!{
+            [If] x:(exp(st)) [Then] y:(exp(st)) [Else] z:(exp(st))
+                { Box::new(Syntax::If(x, y, z)) }
+            [Let] [Rec] [Ident(n)] formal_args:[Ident(_)]+ [Equal] e0:exp(st)
+                [In] e1:exp(st)
                 {
-                    Box::new( Syntax::LetRec( Fundef {
+                    Box::new(Syntax::LetRec(Fundef {
                             // FIXME: fresh tyvar
-                            name : ( String::from( n ), Type::Var( None ) ),
+                            name : (String::from(n), Type::Var(None)),
                             args : formal_args.into_iter().map(|tok| {
                                     if let Ident(x) = tok {
                                         // FIXME: ID, fresh tyvar
-                                        ( String::from( x ), Type::Var( None ) )
+                                        (String::from(x), Type::Var(None))
                                     } else {
                                         unreachable!()
                                     }
-                                } ).collect(),
+                                }).collect(),
                             body : e0 },
-                        e1 ) )
+                        e1))
                 }
-            [Let] [Ident(n)] [Equal] e0:(exp( st ))
+            [Let] [Ident(n)] [Equal] e0:(exp(st))
                 // note: below is how to add state
                 // can use with immutable data structs for e.g. environment
                 //[In] e1:(exp( { let st2 = (); st2 } ))
                 [In] e1:exp(st)
-                { Box::new(
-                    Syntax::Let( (  String::from( n ),
-                                    Type::Var( None ) // FIXME: fresh type var
-                                ), e0, e1 ) )
+                // FIXME: fresh type var
+                { Box::new(Syntax::Let((String::from(n), Type::Var(None)),
+                    e0, e1))
                 }
             [Let] [LParen] ids:([Ident(_)] **<2,> [Comma]) [RParen]
                 [Equal] e0:exp(st) [In] e1:exp(st)
             {
                 // FIXME: patterns are not recursive?
-                Box::new( Syntax::LetTuple(
+                Box::new(Syntax::LetTuple(
                     ids.into_iter().map(|tok| {
+                            // FIXME: Id, fresh tyvar
                             if let Ident(x) = tok {
-                                // FIXME: Id, fresh tyvar
-                                ( String::from(x), Type::Var(None) )
+                                (String::from(x), Type::Var(None))
                             } else {
                                 unreachable!()
                             }
-                        } ).collect(),
-                    e0, e1 ) )
+                        }).collect(),
+                    e0, e1))
             }
             // FIXME: This corresponse to the `elems` rule in parser.mly
             // which doesn't require the parentheses...
             [LParen] e0:exp( st ) [Comma]
-                es:(exp( st ) ++ [Comma]) [RParen]
+                es:(exp(st) ++ [Comma]) [RParen]
                 {
                     let mut vec = Vec::new();
                     vec.push(e0);
                     vec.extend(es);
-                    Box::new( Syntax::Tuple(vec) )
+                    Box::new(Syntax::Tuple(vec))
                 }
             x:(@) [Semicolon] y:@
             {
-                Box::new( Syntax::Let(
-                    ( /* FIXME: fresh tmp id */ String::from(""), Type::Unit ),
-                        x, y )
-                )
+                // FIXME: fresh tmp id
+                Box::new(Syntax::Let((String::from(""), Type::Unit), x, y))
             }
-            [ArrayCreate] e0:simple_exp( st ) e1:simple_exp( st )
+            [ArrayCreate] e0:simple_exp(st) e1:simple_exp(st)
             {
-                Box::new( Syntax::Array( e0, e1 ) )
+                Box::new(Syntax::Array(e0, e1))
             }
-            e0:simple_exp( st ) [Dot] [LParen] e1:exp( st ) [RParen]
-                [LessMinus] e2:exp( st )
-            { Box::new( Syntax::Put( e0, e1, e2 ) ) }
+            e0:simple_exp(st) [Dot] [LParen] e1:exp(st) [RParen]
+                [LessMinus] e2:exp(st)
+            { Box::new(Syntax::Put(e0, e1, e2)) }
             // e0:(@) [Dot] [LParen] e1:exp( st ) [RParen]
             //     [LessMinus] e2:exp( st )
             // { Box::new( Syntax::Put( e0, e1, e2 ) ) }
             --
-            x:(@) [Equal] y:@
-                { Box::new( Syntax::Eq( x, y ) ) }
+            x:(@) [Equal] y:@           { Box::new(Syntax::Eq(x, y)) }
             x:(@) [LessGreater] y:@
                 // FIXME: specific constructor?
-                { Box::new( Syntax::Not( Box::new( Syntax::Eq( x, y ) ) ) ) }
-            x:(@) [Less] y:@
-                { Box::new( Syntax::Lt( x, y ) ) }
-            x:(@) [Greater] y:@
-                { Box::new( Syntax::Gt( x, y ) ) }
-            x:(@) [LessEqual] y:@
-                { Box::new( Syntax::Le( x, y ) ) }
-            x:(@) [GreaterEqual] y:@
-                { Box::new( Syntax::Ge( x, y ) ) }
+                { Box::new(Syntax::Not(Box::new(Syntax::Eq(x, y)))) }
+            x:(@) [Less] y:@            { Box::new(Syntax::Lt(x, y)) }
+            x:(@) [Greater] y:@         { Box::new(Syntax::Gt(x, y)) }
+            x:(@) [LessEqual] y:@       { Box::new(Syntax::Le(x, y)) }
+            x:(@) [GreaterEqual] y:@    { Box::new(Syntax::Ge(x, y)) }
             --
-            x:(@) [Plus] y:@
-                { Box::new( Syntax::Add( x, y ) ) }
-            x:(@) [Minus] y:@
-                { Box::new( Syntax::Sub( x, y ) ) }
-            x:(@) [PlusDot] y:@
-                { Box::new( Syntax::FAdd( x, y ) ) }
-            x:(@) [MinusDot] y:@
-                { Box::new( Syntax::FSub( x, y ) ) }
+            x:(@) [Plus] y:@            { Box::new(Syntax::Add(x, y)) }
+            x:(@) [Minus] y:@           { Box::new(Syntax::Sub(x, y)) }
+            x:(@) [PlusDot] y:@         { Box::new(Syntax::FAdd(x, y)) }
+            x:(@) [MinusDot] y:@        { Box::new(Syntax::FSub(x, y)) }
             --
-            x:(@) [AstDot] y:@
-                { Box::new( Syntax::FMul( x, y ) ) }
-            x:(@) [SlashDot] y:@
-                { Box::new( Syntax::FDiv( x, y ) ) }
+            x:(@) [AstDot] y:@          { Box::new(Syntax::FMul(x, y)) }
+            x:(@) [SlashDot] y:@        { Box::new(Syntax::FDiv(x, y)) }
             --
-            [Minus] x:(@)               { Box::new( Syntax::Neg( x ) ) }
-            [MinusDot] x:(@)            { Box::new( Syntax::FNeg( x ) ) }
+            [Minus] x:(@)               { Box::new(Syntax::Neg(x)) }
+            [MinusDot] x:(@)            { Box::new(Syntax::FNeg(x)) }
             --
             // [LParen] [RParen]           { Box::new( Syntax::Unit ) }
             // [Bool(x)]                   { Box::new( Syntax::Bool(x) ) }
@@ -141,16 +121,13 @@ peg::parser! {
             //     { Box::new( Syntax::Var( Box::new( () ) ) ) }
             // [LParen] e:exp( st ) [RParen]   { e }
             // --
-            e:app_exp( st )             { e }
-            e:simple_exp( st )          { e }
+            e:app_exp(st)               { e }
+            e:simple_exp(st)            { e }
         }
 
         // FIXME: merge with above?
-        pub rule app_exp( st : () ) -> Box<Syntax>
-            = e0:simple_exp( st ) es:(exp( st ))+
-            {
-                Box::new( Syntax::App( e0, es ) )
-            }
+        pub rule app_exp(st : ()) -> Box<Syntax>
+            = e0:simple_exp(st) es:(exp(st))+ { Box::new(Syntax::App(e0, es)) }
 
     }
 }
