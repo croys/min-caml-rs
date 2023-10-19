61db70f8a997d12b6455e44f3718da23baeeb519
Stuart Croy <stuartcroy@mac.com>
2023-10-19 12:02:31 +0100

    test.typing: Fix for unecessary mut refs.

--- src/test/typing.rs
+++ src/test/typing.rs
@@ -35,24 +35,24 @@ fn test_understanding() {
 
 #[test]
 fn test_deref_typ() {
-    assert_eq!(Type::Unit, typing::deref_typ(&mut Type::Unit));
-    assert_eq!(Type::Bool, typing::deref_typ(&mut Type::Bool));
-    assert_eq!(Type::Int, typing::deref_typ(&mut Type::Int));
-    assert_eq!(Type::Float, typing::deref_typ(&mut Type::Float));
+    assert_eq!(Type::Unit, typing::deref_typ(&Type::Unit));
+    assert_eq!(Type::Bool, typing::deref_typ(&Type::Bool));
+    assert_eq!(Type::Int, typing::deref_typ(&Type::Int));
+    assert_eq!(Type::Float, typing::deref_typ(&Type::Float));
 
     let f_ty = Type::Fun(vec![Type::Int], Box::new(Type::Int));
-    assert_eq!(f_ty, typing::deref_typ(&mut f_ty.clone()));
+    assert_eq!(f_ty, typing::deref_typ(&f_ty.clone()));
     
     let tup_ty = Type::Tuple(vec![Type::Int, Type::Float]);
-    assert_eq!(tup_ty, typing::deref_typ(&mut tup_ty.clone()));
+    assert_eq!(tup_ty, typing::deref_typ(&tup_ty.clone()));
 
     let arr_ty = Type::Array(Box::new(Type::Int));
-    assert_eq!(arr_ty, typing::deref_typ(&mut arr_ty.clone()));
+    assert_eq!(arr_ty, typing::deref_typ(&arr_ty.clone()));
 
     // Test default of unbound tyvars
     {
-        let mut var_ty = ty::gentyp();
-        let var_ty2 = typing::deref_typ(&mut var_ty);
+        let var_ty = ty::gentyp();
+        let var_ty2 = typing::deref_typ(&var_ty);
 
         println!("var_ty: {:?}", var_ty);
         println!("var_ty2: {:?}", var_ty2);
@@ -61,7 +61,7 @@ fn test_deref_typ() {
         assert_eq!(expected_var_ty, var_ty);
         assert_eq!(Type::Int, var_ty2);
 
-        let var_ty3 = typing::deref_typ(&mut var_ty);
+        let var_ty3 = typing::deref_typ(&var_ty);
         println!("var_ty3: {:?}", var_ty3);
 
         assert_eq!(expected_var_ty, var_ty);
@@ -70,8 +70,8 @@ fn test_deref_typ() {
 
     // test bound tyvar
     {
-        let mut var_ty = Type::Var(Rc::new(RefCell::new(Some(Type::Float))));
-        let var_ty2 = typing::deref_typ(&mut var_ty);
+        let var_ty = Type::Var(Rc::new(RefCell::new(Some(Type::Float))));
+        let var_ty2 = typing::deref_typ(&var_ty);
 
         println!("var_ty: {:?}", var_ty);
         println!("var_ty2: {:?}", var_ty2);
@@ -80,7 +80,7 @@ fn test_deref_typ() {
         assert_eq!(expected_var_ty, var_ty);
         assert_eq!(Type::Float, var_ty2);
 
-        let var_ty3 = typing::deref_typ(&mut var_ty);
+        let var_ty3 = typing::deref_typ(&var_ty);
         println!("var_ty3: {:?}", var_ty3);
 
         assert_eq!(expected_var_ty, var_ty);
@@ -90,9 +90,9 @@ fn test_deref_typ() {
 
     // test unbound tyvar under constructor
     {
-        let mut var_ty = Type::Array(Box::new(ty::gentyp()));
+        let var_ty = Type::Array(Box::new(ty::gentyp()));
         println!("var_ty: {:?}", var_ty);
-        let var_ty2 = typing::deref_typ(&mut var_ty);
+        let var_ty2 = typing::deref_typ(&var_ty);
         println!("var_ty: {:?}", var_ty);
         println!("var_ty2: {:?}", var_ty2);
 
@@ -105,10 +105,10 @@ fn test_deref_typ() {
 
     // test bound tyvar under constructor
     {
-        let mut var_ty = Type::Array(Box::new(
+        let var_ty = Type::Array(Box::new(
             Type::Var(Rc::new(RefCell::new(Some(Type::Float))))));
         println!("var_ty: {:?}", var_ty);
-        let var_ty2 = typing::deref_typ(&mut var_ty);
+        let var_ty2 = typing::deref_typ(&var_ty);
 
         println!("var_ty: {:?}", var_ty);
         println!("var_ty2: {:?}", var_ty2);
