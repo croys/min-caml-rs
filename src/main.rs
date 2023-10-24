115d2a842eda8d02771840204e8fe5b13a718cb3
Stuart Croy <stuartcroy@mac.com>
2023-10-24 18:14:12 +0100

    main: Simple command line interface to allow dumping output of compiler at various stages.

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,72 @@ mod typing;
 #[cfg(test)]
 mod test;
 
+use clap::Parser;
+use std::fmt;
+use std::fs;
+
+#[derive(Parser, Debug, PartialEq, Eq, Copy, Clone)]
+enum Stage {
+    Lex,
+    Parse,
+    Type,
+    Normal,
+}
+
+impl fmt::Display for Stage {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl<'s> From<&'s str> for Stage {
+    fn from(s: &str) -> Stage {
+        let mut s_ = String::from(s);
+        s_.make_ascii_lowercase();
+        match s_.as_str() {
+            "lex" => Stage::Lex,
+            "parse" => Stage::Parse,
+            "type" => Stage::Type,
+            "normal" => Stage::Normal,
+            _ => panic!("Unknown stage"),
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Source file
+    filename: String,
+
+    /// dump compiler output as of given stage: Lex, Parse, Type, Normal
+    #[arg(short, long, default_value_t = Stage::Normal)]
+    stage: Stage,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let contents =
+        fs::read_to_string(args.filename).expect("Unable to read file");
+    let lexemes =
+        lexer::parser::main(&contents).expect("Lexical analysis failed");
+    if args.stage == Stage::Lex {
+        println!("{:?}", lexemes);
+        return;
+    }
+    let ast = parser::parser::exp(&lexemes, ()).expect("Parsing failed");
+    if args.stage == Stage::Parse {
+        println!("{:?}", ast);
+        return;
+    }
+    let typed_ast = typing::f(&ast).expect("Type inference failed");
+    if args.stage == Stage::Type {
+        println!("{:?}", typed_ast);
+        // FIXME: dump extenv
+        return;
+    }
+    let norm_exp = k_normal::f(&typed_ast);
+    // FIXME: dump extenv
+    println!("{:?}", norm_exp);
 }
