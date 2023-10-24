0085dd42d4c6f0aea87bb31140498300edb1d07e
Stuart Croy <stuartcroy@mac.com>
2023-10-24 12:24:13 +0100

    WIP k_normal: Initial version of translation for application of external functions

--- src/k_normal.rs
+++ src/k_normal.rs
@@ -4,6 +4,7 @@ use crate::id;
 use crate::syntax;
 use crate::syntax::Syntax;
 use crate::ty::Type;
+use crate::typing;
 
 //use im;
 
@@ -229,7 +230,6 @@ pub fn g(env: &im::HashMap<id::T, Type>, e: &Syntax) -> (T, Type) {
             },
             e2,
         ) => {
-            // FIXME: Syntax should use id::T
             let mut env_ = env.update(x.clone(), t.clone());
             let (e2_, t2) = g(&env_, e2);
             env_.extend(yts.iter().map(|(y, t)| (y.clone(), t.clone())));
@@ -248,6 +248,97 @@ pub fn g(env: &im::HashMap<id::T, Type>, e: &Syntax) -> (T, Type) {
                 ),
                 t2,
             )
+        }
+        S::App(e1, e2s) => {
+            match **e1 {
+                // 外部関数の呼び出し
+                // - external variable invocation
+                S::Var(ref f) if !env.contains_key(f) => {
+                    // Note:
+                    // The Caml code is building an expression:
+                    //
+                    // let x0 = e0 in
+                    //    let x1 = e1 in
+                    //      ...
+                    //        let xN = eN in
+                    //          ExtFunApp f x0, x1, ... xN
+                    //
+                    // where no `let` is necessary if the expression is a
+                    // variable
+                    //
+                    // Rust's lack of recursive closures and partial
+                    // application makes it difficult to directly translate
+                    // the Caml without hacks or lambda lifting and lots
+                    // of intrusive messy mechanism. So, lets just do
+                    // a simple imperative equivalent.
+                    //
+                    // gather ids and types: (g env eX)
+                    // generate ids upfront, then fold over expressions
+                    // - need tag to indicate whether let is necessary
+                    //
+                    // [(id, needs_let)]
+                    //
+                    // just use (id, normalised_exp, type)
+                    // can check if normalised_exp is Var, then: no let needed
+
+                    // Ugh, Rust doesn't have recursive closures
+                    // Ugh, no partial application either...
+                    // write the equivalent as a loop, don't use
+                    // insert_let
+                    // fn bind(f : &id::T, xs : &Vec<Syntax>, ys : &Vec<Syntax> ) -> T {
+                    //     todo!()
+                    // }
+
+                    typing::EXTENV.with(|extenv_| {
+                        let extenv = extenv_.borrow();
+                        match extenv.get(f) {
+                            Some(Type::Fun(_, t)) => {
+                                // FIXME: make this a local convenience fn
+
+                                // normalise expressions and generate ids
+                                // as necessary
+                                let nes = e2s.iter().map(|e2| {
+                                    let (e2_, e2_t) = g(env, e2);
+                                    match e2_ {
+                                        Var(ref x) => (x.clone(), e2_, e2_t),
+                                        _ => {
+                                            let x = id::gentmp(&e2_t);
+                                            (x, e2_, e2_t)
+                                        }
+                                    }
+                                });
+
+                                // base expression is external call
+                                // using all ids
+                                let re0 = ExtFunApp(
+                                    f.clone(),
+                                    nes.clone().map(|(x, _, _)| x).collect(),
+                                );
+
+                                // iterate right to left
+                                // to build necessary let expressions
+                                let mut res = re0;
+                                for (id, e, t) in nes.rev() {
+                                    if let Var(x) = e {
+                                        // no need for let
+                                    } else {
+                                        // insert let
+                                        res = Let((id, t), b(e), b(res));
+                                    }
+                                }
+                                (res, *t.clone())
+                            }
+                            // Caml code does `assert false`
+                            // FIXME: use a Result
+                            _ => unreachable!(),
+                        }
+                    })
+                }
+                _ => {
+                    // as above, but we have a function expr
+                    todo!()
+                }
+            }
         }
         _ => todo!(),
     }
